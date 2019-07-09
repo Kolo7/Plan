078849251826e820801c0c82f868ee010309cba2
kolo <1438349140@qq.com>
2019-07-10 02:22:28 +0800

    加回来场景，加入战机切换

--- Plane/Assets/Scripts/ModelManager.cs
+++ Plane/Assets/Scripts/ModelManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ModelManager : MonoBehaviour {
 
 	public GameObject[] playerModel;
-	public static int modelIndex;
+	public static int modelIndex=0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,4 +17,16 @@ public class ModelManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	public void GameStart(){
+		SceneManager.LoadScene("plane");
+	}
+
+	public void Next(){
+		Debug.Log (modelIndex);
+		playerModel [modelIndex].gameObject.SetActive (false);
+		modelIndex = (modelIndex + 1) % 2;
+		playerModel [modelIndex].gameObject.SetActive (true);
+	}
+
 }
--- Plane/Assets/Scripts/UIManager.cs
+++ Plane/Assets/Scripts/UIManager.cs
@@ -46,7 +46,7 @@ public class UIManager : MonoBehaviour {
 	public void StartGame()
 	{
 		
-		SceneManager.LoadScene("plane");
+		SceneManager.LoadScene("scene");
 
 	}
 
